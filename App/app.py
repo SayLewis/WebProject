1165e8fcd75c8ac3ecad0c4096077c7fa9c355bb
SayLewis <nickelllewis1@gmail.com>
2025-03-31 20:24:45 -0400

    This should be our based guide setup for rn

---
+++ App/app.py
@@ -0,0 +1,72 @@
+import os, csv
+import datetime
+from flask import Flask, request, redirect, render_template, url_for, flash
+from flask_cors import CORS
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    set_access_cookies,
+    unset_jwt_cookies,
+    current_user
+)
+from .models import *
+# Configure Flask App
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MySecretKey'
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
+app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token'
+app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(hours=15)
+app.config["JWT_COOKIE_SECURE"] = True
+app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+app.config['JWT_HEADER_NAME'] = "Cookie"
+
+
+# Initialize App 
+db.init_app(app)
+app.app_context().push()
+CORS(app)
+jwt = JWTManager(app)
+
+# JWT Config to enable current_user
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+  return user.id
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+  identity = jwt_data["sub"]
+  return User.query.get(identity)
+
+
+#To set up the database
+def initialize_db(app):
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+        # Create default user 'bob'
+        bob = User(
+            username='bob',
+            email='bob@mail.com',
+            password=generate_password_hash('bobpass')
+        )
+        db.session.add(bob)
+        db.session.commit()
+
+        print("📦 Database initialized and default user 'bob' created.")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
